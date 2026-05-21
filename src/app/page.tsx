b51bcc5433d6e5c86065c3c877406fb6064d9a08
andersongama-dev <andersongamasilva5@gmail.com>
2026-05-21 14:40:33 -0300

    feat: footer section home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -656,6 +656,64 @@ export default function Home() {
           Agendar demonstração
         </Link>
       </section>
+
+      <footer className="w-full grid-cols-1 gap-12 mt-16 px-6 md:px-10 lg:px-16">
+        <div className="flex gap-32">
+          <ul className="flex flex-col gap-6">
+            <h5 className="text-xl text-[#B1B4B8] font-medium">Links</h5>
+            <div className="flex flex-col gap-4">
+              <li>
+                <Link href="/">Recursos</Link>
+              </li>
+
+              <li>
+                <Link href="/">Soluções</Link>
+              </li>
+
+              <li>
+                <Link href="/">Preços</Link>
+              </li>
+
+              <li>
+                <Link href="/">Contato</Link>
+              </li>
+            </div>
+          </ul>
+
+          <ul className="flex flex-col gap-6">
+            <h5 className="text-xl text-[#B1B4B8] font-medium">Sobre</h5>
+            <div className="flex flex-col gap-4">
+              <li>
+                <Link href="/">Termos de uso</Link>
+              </li>
+
+              <li>
+                <Link href="/">Dúvidas frequentes</Link>
+              </li>
+            </div>
+          </ul>
+
+          <ul className="flex flex-col gap-6">
+            <h5 className="text-xl text-[#B1B4B8] font-medium">Contato</h5>
+            <div className="flex flex-col gap-4">
+              <li>
+                <Link href="/">Email: suporte@igniscore.com</Link>
+              </li>
+
+              <li>
+                <Link href="/">Telefone: (19) 9967-79283</Link>
+              </li>
+            </div>
+          </ul>
+        </div>
+
+        <div className="mt-8 border-t border-[#B1B4B8] py-8">
+          <p className="text-lg leading-normal tracking-[0.02em] text-center">
+            © {new Date().getFullYear()} IgnisCore. Todos os direitos
+            reservados.
+          </p>
+        </div>
+      </footer>
     </div>
   );
 }
