43f875e12324c108222e8e2748a8a490f3c04a6c
andersongama-dev <andersongamasilva5@gmail.com>
2026-05-21 19:37:51 -0300

    Separation of the benefits section and scroll animation and UI/UX adjustments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Challenges from "@/components/home/challenges";
 import Header from "@/components/home/header";
 import Hero from "@/components/home/Hero";
 import InteractionDashboard from "@/components/home/interaction-dashboard";
+import Result from "@/components/home/results";
 import Solutions from "@/components/home/solutions";
 import {
   ArrowRight,
@@ -30,53 +31,7 @@ export default function Home() {
 
       <InteractionDashboard />
 
-      <section className="grid justify-center w-full grid-cols-1 gap-12 mt-16 px-6 md:px-10 lg:px-16">
-        <div className="flex w-full">
-          <h2 className="text-4xl md:text-5xl text-[#B1B4B8] font-medium leading-[1.3] tracking-[0.01em] max-w-full">
-            <span className="text-[#FF5A1F]">Resultados</span> que impactam sua
-            <span className="block">operação</span>
-          </h2>
-        </div>
-
-        <div className="flex gap-16">
-          <article className="w-110">
-            <div className="flex flex-col gap-4 py-6 border-b border[#B1B4B8]">
-              <h4 className="text-2xl text-[#FF5A1F] font-medium">
-                Redução de atrasos
-              </h4>
-              <p>
-                Yorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
-                vulputate libero et velit interdum, ac aliquet odio mattis.
-              </p>
-            </div>
-            <div className="py-6 border-b border[#B1B4B8]">
-              <h4 className="text-xl text-[#B1B4B8] font-medium">
-                Mais produtividade
-              </h4>
-            </div>
-            <div className="py-6 border-b border[#B1B4B8]">
-              <h4 className="text-xl text-[#B1B4B8] font-medium">
-                Melhor atendimento
-              </h4>
-            </div>
-            <div className="py-6 border-b border[#B1B4B8]">
-              <h4 className="text-xl text-[#B1B4B8] font-medium">
-                Gestão centralizada
-              </h4>
-            </div>
-          </article>
-
-          <article>
-            <Image
-              src="/next.svg"
-              alt="Hero Image"
-              width={1287}
-              height={771}
-              className="h-192.75"
-            ></Image>
-          </article>
-        </div>
-      </section>
+      <Result />
 
       <section className="grid justify-center w-full grid-cols-1 gap-12 mt-16 px-6 md:px-10 lg:px-16">
         <div className="flex justify-center w-full">
