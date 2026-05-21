eeabe01f0e6e5d73408cdc840b7ff5f7fb9cf8ce
andersongama-dev <andersongamasilva5@gmail.com>
2026-05-21 09:35:40 -0300

    feat: section line temp and result & operations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -362,6 +362,198 @@ export default function Home() {
           </article>
         </div>
       </section>
+
+      <section className="grid justify-center w-full grid-cols-1 gap-12 mt-16 px-6 md:px-10 lg:px-16">
+        <div className="flex justify-center w-full">
+          <h2 className="text-4xl md:text-5xl text-[#B1B4B8] font-medium leading-[1.3] tracking-[0.01em] max-w-full lg:max-w-[50dvw] text-center">
+            <span className="text-[#FF5A1F]">Visualize sua</span> operação em
+            <span className="block">tempo real</span>
+          </h2>
+        </div>
+
+        <Image
+          src="/next.svg"
+          alt="Hero Image"
+          width={0}
+          height={0}
+          className="object-contain w-full mt-12"
+        ></Image>
+      </section>
+
+      <section className="grid justify-center w-full grid-cols-1 gap-12 mt-16 px-6 md:px-10 lg:px-16">
+        <div className="flex w-full">
+          <h2 className="text-4xl md:text-5xl text-[#B1B4B8] font-medium leading-[1.3] tracking-[0.01em] max-w-full">
+            <span className="text-[#FF5A1F]">Resultados</span> que impactam sua
+            <span className="block">operação</span>
+          </h2>
+        </div>
+
+        <div className="flex gap-16">
+          <article className="w-110">
+            <div className="flex flex-col gap-4 pb-6">
+              <h4 className="text-2xl text-[#FF5A1F] font-medium">
+                Redução de atrasos
+              </h4>
+              <p>
+                Yorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+                vulputate libero et velit interdum, ac aliquet odio mattis.
+              </p>
+            </div>
+            <div className="py-6 border-y border[#B1B4B8]">
+              <h4 className="text-xl text-[#B1B4B8] font-medium">
+                Mais produtividade
+              </h4>
+            </div>
+            <div className="py-6 border-y border[#B1B4B8]">
+              <h4 className="text-xl text-[#B1B4B8] font-medium">
+                Melhor atendimento
+              </h4>
+            </div>
+            <div className="py-6 border-y border[#B1B4B8]">
+              <h4 className="text-xl text-[#B1B4B8] font-medium">
+                Gestão centralizada
+              </h4>
+            </div>
+          </article>
+
+          <article>
+            <Image
+              src="/next.svg"
+              alt="Hero Image"
+              width={1287}
+              height={771}
+              className="h-192.75"
+            ></Image>
+          </article>
+        </div>
+      </section>
+
+      <section className="grid justify-center w-full grid-cols-1 gap-12 mt-16 px-6 md:px-10 lg:px-16">
+        <div className="flex justify-center w-full">
+          <h2 className="text-4xl md:text-5xl text-[#B1B4B8] font-medium leading-[1.3] tracking-[0.01em] max-w-full lg:max-w-[50dvw] text-center">
+            <span className="text-[#FF5A1F]">Comece</span> em poucos passos
+          </h2>
+        </div>
+
+        <div>
+          <article>
+            <div className="flex gap-32">
+              <div className="flex gap-6 p-8 border border-[#B1B4B8]">
+                <div className="flex gap-6 flex-col">
+                  <div className="flex gap-4">
+                    <Package
+                      width={32}
+                      height={32}
+                      className="text-[#B1B4B8]"
+                    />
+                    <h5 className="text-2xl font-medium text-[#B1B4B8]">
+                      Cadastre sua empresa
+                    </h5>
+                  </div>
+                  <p className="text-[#B1B4B8]">
+                    Norem ipsum dolor sit amet, consectetur adipiscing elit.
+                  </p>
+                </div>
+                <div>
+                  <p className="text-5xl text-[#B1B4B8] font-medium border-l pl-6 border-[#B1B4B8] h-full flex items-center">
+                    1
+                  </p>
+                </div>
+              </div>
+
+              <div className="flex gap-6 p-8 border border-[#B1B4B8]">
+                <div className="flex gap-6 flex-col">
+                  <div className="flex gap-4">
+                    <Package
+                      width={32}
+                      height={32}
+                      className="text-[#B1B4B8]"
+                    />
+                    <h5 className="text-2xl font-medium text-[#B1B4B8]">
+                      Automatize vencimentos
+                    </h5>
+                  </div>
+                  <p className="text-[#B1B4B8]">
+                    Norem ipsum dolor sit amet, consectetur adipiscing elit.
+                  </p>
+                </div>
+                <div>
+                  <p className="text-5xl text-[#B1B4B8] font-medium border-l pl-6 border-[#B1B4B8] h-full flex items-center">
+                    3
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div className="relative flex items-center justify-between mx-auto my-6">
+              <div className="absolute top-2 left-0 w-full h-px bg-[#B1B4B8]"></div>
+
+              <div className="relative left-50 z-10 flex flex-col items-center">
+                <div className="w-4 h-4 bg-[#FF5A1F] rounded-full"></div>
+              </div>
+              <div className="relative left-30 z-10 flex flex-col items-center">
+                <div className="w-4 h-4 bg-[#FF5A1F] rounded-full"></div>
+              </div>
+
+              <div className="relative z-10 flex flex-col items-center">
+                <div className="w-4 h-4 bg-[#FF5A1F] rounded-full"></div>
+              </div>
+
+              <div className="relative -left-50 z-10 flex flex-col items-center">
+                <div className="w-4 h-4 bg-[#FF5A1F] rounded-full"></div>
+              </div>
+            </div>
+
+            <div className="flex gap-32 justify-end">
+              <div className="flex gap-6 p-8 border border-[#B1B4B8]">
+                <div className="flex gap-6 flex-col">
+                  <div className="flex gap-4">
+                    <Package
+                      width={32}
+                      height={32}
+                      className="text-[#B1B4B8]"
+                    />
+                    <h5 className="text-2xl font-medium text-[#B1B4B8]">
+                      Cadastre sua empresa
+                    </h5>
+                  </div>
+                  <p className="text-[#B1B4B8]">
+                    Norem ipsum dolor sit amet, consectetur adipiscing elit.
+                  </p>
+                </div>
+                <div>
+                  <p className="text-5xl text-[#B1B4B8] font-medium border-l pl-6 border-[#B1B4B8] h-full flex items-center">
+                    2
+                  </p>
+                </div>
+              </div>
+
+              <div className="flex gap-6 p-8 border border-[#B1B4B8]">
+                <div className="flex gap-6 flex-col">
+                  <div className="flex gap-4">
+                    <Package
+                      width={32}
+                      height={32}
+                      className="text-[#B1B4B8]"
+                    />
+                    <h5 className="text-2xl font-medium text-[#B1B4B8]">
+                      Automatize vencimentos
+                    </h5>
+                  </div>
+                  <p className="text-[#B1B4B8]">
+                    Norem ipsum dolor sit amet, consectetur adipiscing elit.
+                  </p>
+                </div>
+                <div>
+                  <p className="text-5xl text-[#B1B4B8] font-medium border-l pl-6 border-[#B1B4B8] h-full flex items-center">
+                    4
+                  </p>
+                </div>
+              </div>
+            </div>
+          </article>
+        </div>
+      </section>
     </div>
   );
 }
